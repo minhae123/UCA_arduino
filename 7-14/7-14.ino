c6750a5c2265338d91d28683df84b55540f594b7
enddnr <109403086+enddnr@users.noreply.github.com>
2022-07-17 17:02:09 +0900

    led--

--- 7-14/7-14.ino
+++ 7-14/7-14.ino
@@ -1,12 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(7, HIGH);
-  delay(1000);
+  digitalWrite(8, HIGH);
+  digitalWrite(2, HIGH);
+  delay(100);
   digitalWrite(7, LOW);
-  delay(1000);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+  delay(100);
 }
