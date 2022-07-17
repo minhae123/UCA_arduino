08d9eed32ff123d6aaff66b0ce94795ca77bf9d0
enddnr <109403086+enddnr@users.noreply.github.com>
2022-07-17 18:52:25 +0900

    pullup

--- switch_digital/switch_digital.ino
+++ switch_digital/switch_digital.ino
@@ -1,9 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(13, INPUT);
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(13)==HIGH){
+    Serial.println("1");
+    digitalWrite(3, HIGH);
+  }else if(digitalRead(13)==LOW){
+    Serial.println("0");
+    digitalWrite(3, LOW);
+  }
 }
