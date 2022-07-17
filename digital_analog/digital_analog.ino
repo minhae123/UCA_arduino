8e2741082d534bab33d279e817baced5f244d096
enddnr <109403086+enddnr@users.noreply.github.com>
2022-07-17 17:44:35 +0900

    analog-led

--- digital_analog/digital_analog.ino
+++ digital_analog/digital_analog.ino
@@ -1,9 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  for(int i=0;i<256;i++){
+    delay(10);
+    analogWrite(3, i);
+    //analogWrite(5, i);
+    //analogWrite(9, i);
+  }
+  for(int i=255;i>0;i--){
+    analogWrite(3, i);
+    //analogWrite(5, i);
+    //analogWrite(9, i);
+    delay(10);
+  }
 }
